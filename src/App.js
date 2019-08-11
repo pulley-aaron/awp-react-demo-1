71845d6dc0cd0ef0b52093cd3087bd956b0a38f5
Aaron Pulley <awpulley1@gmail.com>
2019-08-10 23:23:40 -0400

    Redux store updating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchResult from './SearchResult';
+import { connect } from 'react-redux';
 
 class App extends React.Component {
     constructor(props) {
@@ -23,47 +24,65 @@ class App extends React.Component {
         // Fetch from API
         var queryString = 'http://hn.algolia.com/api/v1/search?query="'+this.state.query+'"';
         fetch(queryString)
-            .then(reply => {return reply.json();})
-            .then(data => {
-                var results = data.hits.map((item, index) => {
-                    return(
-                        <SearchResult
-                        key={index}
-                        url={item.url}
-                        title={item.title}
-                        />
-                    );
-                });
-                // This could be enhanced to filter out results with no URL.
-                this.setState({results: results});
+        .then(reply => {return reply.json();})
+        .then(data => {
+            var results = data.hits.map((item, index) => {
+                return(
+                    <SearchResult
+                    key={index}
+                    url={item.url}
+                    title={item.title}
+                    />
+                );
             });
+            // This could be enhanced to filter out results with no URL.
+            this.setState({results: results});
+            this.props.updateSearch(this.state.query, results);
+            console.log("results via connection",this.props.results);
+        });
     }
     
     render() {
         return(
-            <div className="App">
-                <form
-                    className="searchBar"
-                    onSubmit={this.handleSubmit}
-                >
-                    <input
-                        type="search"
-                        name="search"
-                        value={this.state.query}
-                        onChange={this.handleChange}
-                    ></input>
-                    <input
-                        type="submit"
-                        value="Search"
-                    ></input>
-                </form>
-                <ul>
-                    {this.state.results}
-                </ul>
-            </div>
+            
+                <div className="App">
+                    <form
+                        className="searchBar"
+                        onSubmit={this.handleSubmit}
+                    >
+                        <input
+                            type="search"
+                            name="search"
+                            value={this.state.query}
+                            onChange={this.handleChange}
+                        ></input>
+                        <input
+                            type="submit"
+                            value="Search"
+                        ></input>
+                    </form>
+                    <ul>
+                        {this.state.results}
+                    </ul>
+                </div>
         );
         // This could be enhanced to separate the search bar into a reusable React component.
     }
 }
 
-export default App;
+// these two functions are required to connect component with Redux store.
+function mapStateToProps(state) {
+    return {
+        query: state.query,
+        results: state.results
+    };
+};
+
+function mapDispatchToProps(dispatch) {
+    return {
+        updateSearch: (query, results) => dispatch({type: "UPDATE_SEARCH", query: query, results: results})
+    };
+    // This could be enhanced by separating the actions into their own files for reusability.
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,5 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Set up Redux
+function reducer(state = {query: "",results: []}, action) {
+    console.log("before", state, action);
+    switch(action.type) {
+        case "UPDATE_SEARCH":
+            state.query = action.query;
+            state.results = action.results;
+            break;
+        // More cases would be added in a more complex program.
+        default:
+            // No default case needed currently, but this removes a warning message.
+    };
+    console.log("after", state);
+    return state;
+}
+const store = createStore(reducer);
+
+// Render App
+ReactDOM.render(
+    <Provider
+        store={store}
+    >
+    <App />
+    </Provider>,
+    document.getElementById('root')
+);
